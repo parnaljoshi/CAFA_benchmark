89c78aa17d131b386c34deef05a6d37ba1bcf2ee
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2019-12-03 15:44:16 -0600

    Update create_benchmark.py

--- create_benchmark.py
+++ create_benchmark.py
@@ -70,6 +70,7 @@ def analyze(t1_dic,t2_dic,all_protein_t1):
                 for ontology in t2_dic[protein]:
                     if ontology not in t1_dic[protein]: # for those lack, include in LK
                         LK_dic[ontology][protein] = t2_dic[protein][ontology]
+    print(NK_dic)
     return NK_dic,LK_dic 
     
 '''
